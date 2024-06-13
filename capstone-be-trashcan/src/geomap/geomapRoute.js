d10e8317d25a4f8926029f0db17765d42b2b3b54
Athira <athiradhya@gmail.com>
2024-06-14 01:08:53 +0700

    delete folder geomap

--- capstone-be-trashcan/src/geomap/geomapRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getNearestWasteBank } = require('./handler');
-
-const router = express.Router();
-
-router.get('/nearest-waste-bank', getNearestWasteBank);
-
-module.exports = router;
