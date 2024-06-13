9fd63791adcf93f56e6a9d82e93c88574b78c16e
Athira <athiradhya@gmail.com>
2024-06-14 01:05:13 +0700

    add server

--- capstone-be-trashcan/server.js
+++ capstone-be-trashcan/server.js
@@ -1,20 +1,25 @@
 const Hapi = require('@hapi/hapi');
 const admin = require('firebase-admin');
-const serviceAccount = require('../capstone-be-trashcan/trashcan-1e54a-firebase-adminsdk-bmm8g-6a60b5925e.json');
-
+const serviceAccount = require('./trashcan-1e54a-firebase-adminsdk-bmm8g-6a60b5925e.json');
+const dotenv = require('dotenv');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
+dotenv.config();
+
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: 8080,
         host: 'localhost'
     });
 
     // routes
     const userRoute = require('./src/route/userRoute');
     server.route(userRoute);
+
+    const geomapRoutes = require('./src/route/geomapRoute');
+    server.route(geomapRoutes);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
